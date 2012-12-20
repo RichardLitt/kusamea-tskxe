8a0968ad6bb70f08b1e992817ca0f603ede67795
Antal Szabó <szabo.antal.92@gmail.com>
2012-12-20 16:02:46 +0100

    Extract inflections too

--- sql_to_json.py
+++ sql_to_json.py
@@ -5,7 +5,14 @@ import json
 
 metaPattern = re.compile(r"INSERT INTO `metaWords` \(`id`,`navi`,`ipa`,`infixes`,`partOfSpeech`\) VALUES \('(\d+)','((?:[^'\\]|\\.)*)','((?:[^'\\]|\\.)*)',(?:'((?:[^'\\]|\\.)*)'|NULL),'((?:[^'\\]|\\.)*)'\);")
 localizedPattern = re.compile(r"INSERT INTO `localizedWords` \(`id`,`languageCode`,`localized`,`partOfSpeech`\) VALUES \('(\d+)','((?:[^'\\]|\\.)*)','((?:[^'\\]|\\.)*)','((?:[^'\\]|\\.)*)'\);")
+
+imetaPattern = re.compile(r"INSERT INTO `metaInfixes` \(`id`, `navi`, `ipa`, `shorthand`, `position`\) VALUES \('(\d+)', '((?:[^'\\]|\\.)*)', '((?:[^'\\]|\\.)*)', '((?:[^'\\]|\\.)*)', (?:'((?:[^'\\]|\\.)*)'|NULL)\);")
+ilocalizedPattern = re.compile(r"INSERT INTO `localizedInfixes` \(`id`, `languageCode`, `meaning`, `habitat`\) VALUES \('(\d+)', '((?:[^'\\]|\\.)*)', '((?:[^'\\]|\\.)*)', (?:'((?:[^'\\]|\\.)*)'|NULL)\);")
+
 unescapePattern = re.compile(r"\\(.)")
+
+def sortFunc(elem):
+	return elem["id"]
 
 sql = ""
 with open("dict.sql", "r", encoding="utf8") as f:
@@ -25,8 +32,27 @@ for found in localizedPattern.finditer(sql):
 		groups[i] = unescapePattern.sub(r"\1", groups[i].strip())
 	words[int(groups[0])]["localized"][groups[1]] = {"languageCode": groups[1], "localized": groups[2], "partOfSpeech": groups[3]}
 
-def sortFunc(elem):
-	return elem["id"]
-
-with open("dict.json", "w", encoding="utf8") as f:
+with open("words.json", "w", encoding="utf8", newline="\n") as f:
 	json.dump(sorted(words.values(), key=sortFunc), f, ensure_ascii=False, indent="\t", sort_keys=True)
+
+positions = set()
+inflections = {}
+for found in imetaPattern.finditer(sql):
+	groups = list(found.groups())
+	for i in range(len(groups)):
+		if groups[i]:
+			groups[i] = unescapePattern.sub(r"\1", groups[i].strip())
+	positions.add(groups[4])
+	inflections[int(groups[0])] = {"id": int(groups[0]), "navi": groups[1], "ipa": groups[2], "shorthand": groups[3], "position": groups[4], "localized": {}}
+
+for found in ilocalizedPattern.finditer(sql):
+	groups = list(found.groups())
+	for i in range(len(groups)):
+		if groups[i]:
+			groups[i] = unescapePattern.sub(r"\1", groups[i].strip())
+	inflections[int(groups[0])]["localized"][groups[1]] = {"languageCode": groups[1], "meaning": groups[2], "habitat": groups[3]}
+
+with open("inflections.json", "w", encoding="utf8", newline="\n") as f:
+	json.dump(sorted(inflections.values(), key=sortFunc), f, ensure_ascii=False, indent="\t", sort_keys=True)
+
+print(positions)
